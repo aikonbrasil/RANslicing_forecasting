3e6999e28647db454eb55182b8da90e617786e79
Dick Carrillo <dcarrillom@gmail.com>
2021-04-22 09:01:11 +0300

    updating forecasting v2

--- forecasting_main.py
+++ forecasting_main.py
@@ -141,7 +141,7 @@ model = LSTM()
 loss_function = nn.MSELoss()
 optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
 
-epochs = 10
+epochs = 50
 
 for i in range(epochs):
     for seq, labels in train_inout_seq:
